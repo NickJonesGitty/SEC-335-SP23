d3e608b320b6c4f89cedfe16b7d962c3a7165988
nicholas <nicholas.jones@mymail.champlain.edu>
2023-01-31 19:26:03 -0500

    Week3 Windows10 .ps1 commit

---
+++ Week3/dnsresolver.ps1
@@ -0,0 +1,8 @@
+﻿param($prefix, $dns)
+
+for ($x=1; $x -le 254; $x++)
+{
+    $address = "$prefix.$x"
+    Resolve-DnsName -DnsOnly $address -Server $dns -ErrorAction Ignore | Select-Object Name, NameHost
+
+}
